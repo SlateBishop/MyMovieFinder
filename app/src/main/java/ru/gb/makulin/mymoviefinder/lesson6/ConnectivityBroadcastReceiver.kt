9cd8ca3a07d952eb130b4fe7007fa0b43b042072
Despair <p.a.makulin@gmail.com>
2021-11-08 20:01:22 +0300

    В ДЗ№6 доработал первый пункт. Вывел пользователю конкретное сообщение потеряна или найдена связь, а не просто про ее изменение.
    Столкнулся с проблемой связанной с контекстом..

--- app/src/main/java/ru/gb/makulin/mymoviefinder/lesson6/ConnectivityBroadcastReceiver.kt
+++ app/src/main/java/ru/gb/makulin/mymoviefinder/lesson6/ConnectivityBroadcastReceiver.kt
@@ -3,15 +3,41 @@ package ru.gb.makulin.mymoviefinder.lesson6
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
 import android.util.Log
 import android.widget.Toast
+import ru.gb.makulin.mymoviefinder.R
+
 
 class ConnectivityBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        val message = intent?.action
-        if (message != null) {
-            Toast.makeText(context, message, Toast.LENGTH_LONG).show()
-            Log.d("mylogs", message)
+        if (intent != null && context != null) {
+            val connectivityState =
+                intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false)
+            if (connectivityState) {
+                onConnectivityStateLost(context)
+            } else {
+                onConnectivityStateFound(context)
+            }
         }
     }
+
+    /*
+    В классе BroadcastReceiver(), который имеет свой контекст, у меня почему то не сразботало
+    при создании логов и тостов использовать this. Пришлось передавать контекст в методы, а в
+    методе onReceive отдельно проверять контекст на null.
+    Не смог разобраться почему так..
+     */
+    private fun onConnectivityStateLost(context: Context) {
+        Log.d(context.getString(R.string.LogTAG), context.getString(R.string.connection_lost_msg))
+        Toast.makeText(context, context.getString(R.string.connection_lost_msg), Toast.LENGTH_LONG)
+            .show()
+    }
+
+    private fun onConnectivityStateFound(context: Context) {
+        Log.d(context.getString(R.string.LogTAG), context.getString(R.string.connection_found_msg))
+        Toast.makeText(context, context.getString(R.string.connection_found_msg), Toast.LENGTH_LONG)
+            .show()
+    }
+
 }
