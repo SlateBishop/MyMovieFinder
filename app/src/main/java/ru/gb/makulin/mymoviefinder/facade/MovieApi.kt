2c6165457f66cc2d10d5c1bea3a089e2c066b3b1
Despair <p.a.makulin@gmail.com>
2021-11-18 23:47:21 +0300

    Начало ДЗ№7 Создал отдельный файл с константами, подготовил MovieApi.kt с запросами через Retrofit.

---
+++ app/src/main/java/ru/gb/makulin/mymoviefinder/facade/MovieApi.kt
@@ -0,0 +1,38 @@
+package ru.gb.makulin.mymoviefinder.facade
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+import ru.gb.makulin.mymoviefinder.utils.*
+
+interface MovieApi {
+
+    @GET(MOVIE_API_URL_END_POINT_TOP_RATED)
+    fun getTopRatedMovies(
+        @Query(MOVIE_API_KEY_NAME) apikey: String,
+        @Query("page") page: Int = 1,
+        @Query("language") lang: String = "ru-RU"
+    )
+
+    @GET(MOVIE_API_URL_END_POINT_NOW_PLAYING)
+    fun getNowPlayingMovies(
+        @Query(MOVIE_API_KEY_NAME) apikey: String,
+        @Query("page") page: Int = 1,
+        @Query("language") lang: String = "ru-RU"
+    )
+
+
+    @GET(MOVIE_API_URL_END_POINT_UPCOMING)
+    fun getUpcomingMovies(
+        @Query(MOVIE_API_KEY_NAME) apikey: String,
+        @Query("page") page: Int = 1,
+        @Query("language") lang: String = "ru-RU"
+    )
+
+    @GET(MOVIE_API_URL_END_POINT_DETAILS)
+    fun getMovieDetails(
+        @Path(MOVIE_API_URL_ID_PATH_DETAILS) movieId: Int,
+        @Query(MOVIE_API_KEY_NAME) apikey: String,
+        @Query("language") lang: String = "ru-RU"
+    )
+}
---
+++ app/src/main/java/ru/gb/makulin/mymoviefinder/utils/Constants.kt
@@ -0,0 +1,9 @@
+package ru.gb.makulin.mymoviefinder.utils
+
+const val BASE_MOVIE_API_URL = "https://api.themoviedb.org/"
+const val MOVIE_API_URL_END_POINT_NOW_PLAYING = "3/movie/now_playing"
+const val MOVIE_API_URL_END_POINT_UPCOMING = "3/movie/upcoming"
+const val MOVIE_API_URL_END_POINT_TOP_RATED = "3/movie/top_rated"
+const val MOVIE_API_URL_END_POINT_DETAILS = "3/movie/{movie_id}"
+const val MOVIE_API_URL_ID_PATH_DETAILS = "movie_id"
+const val MOVIE_API_KEY_NAME = "api_key"
