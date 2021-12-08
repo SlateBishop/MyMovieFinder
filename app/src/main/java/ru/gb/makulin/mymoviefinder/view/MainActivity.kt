c672ac373fc664ae29af926d2156784a4217c291
Despair <p.a.makulin@gmail.com>
2021-12-08 23:19:41 +0300

    Начал ДЗ№8. Добавил в меню чекбокс на отображение контента для взрослых и сохранение выбранного параметра в SP

--- app/src/main/java/ru/gb/makulin/mymoviefinder/view/MainActivity.kt
+++ app/src/main/java/ru/gb/makulin/mymoviefinder/view/MainActivity.kt
@@ -1,11 +1,13 @@
 package ru.gb.makulin.mymoviefinder.view
 
+import android.content.Context
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import ru.gb.makulin.mymoviefinder.R
+import ru.gb.makulin.mymoviefinder.utils.SP_IS_ADULT
 import ru.gb.makulin.mymoviefinder.view.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -19,18 +21,27 @@ class MainActivity : AppCompatActivity() {
         initToolbar()
     }
 
+    private val sharedPreferences by lazy {
+        this.getPreferences(Context.MODE_PRIVATE)
+    }
+
     private fun initToolbar() {
         setSupportActionBar(findViewById(R.id.toolbar))
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
+
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.actionHome -> replaceContainerToFragment(MainFragment.newInstance())
+            R.id.isAdultSettings -> {
+                item.isChecked = !item.isChecked
+                sharedPreferences.edit().putBoolean(SP_IS_ADULT, item.isChecked).apply()
+            }
         }
         return super.onOptionsItemSelected(item)
     }
